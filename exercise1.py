ec9a1146331d4e8a1f9112a0d64d6e7fb5c34005
Eric Bréchemier <eric@bezen.org>
2013-09-26 16:11:38 +0200

    Complete exercise 1

--- exercise1.py
+++ exercise1.py
@@ -22,21 +22,16 @@ assert_note_has_name(1,"C#")
 assert_note_has_name(13,"C#")
 assert_note_has_name(3,"D#")
 
-def accidentals(note_string):
-  acc = len(note_string[1:])
-  if "#" in note_string:
-    return acc
-  elif "b" in note_string:
-    return -acc
-  else:
-    return 0
-
 def name_to_number(note_string):
   notes = "C . D . E F . G . A . B".split()
   name = note_string[0:1].upper()
   number = notes.index(name)
-  acc = accidentals(note_string)
-  return mod12(number + acc)
+  for accidental in note_string[1:]:
+    if accidental == "#":
+      number += 1
+    elif accidental == "b":
+      number -= 1
+  return mod12(number)
 
 def assert_name_is_note(name,number):
   actualNumber = name_to_number(name)
