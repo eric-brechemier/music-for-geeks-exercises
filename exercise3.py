d1af12707914bd305a87da6c9987c9f9f58eb4c8
Eric Bréchemier <eric@bezen.org>
2013-10-01 17:16:01 +0200

    Complete exercise 3
    I extracted the number of beats per measure and the reciprocal unity value
    from the time signature string, then computed the duration of a beat and
    multiplied by the total number of beats in the music.
    
    I then converted the result (previously in seconds) to a duration in minutes.

--- exercise3.py
+++ exercise3.py
@@ -12,6 +12,7 @@
 
 from __future__ import division
 
+# Note duration in seconds
 def note_duration(note_value, unity, tempo):
   return (60.0 * note_value) / (tempo * unity)
 
@@ -29,9 +30,19 @@ assert_note_duration_is(1/4, 0.66)
 assert_note_duration_is(1/2, 1.33)
 assert_note_duration_is(1/8, 0.33)
 
+# Music duration in minutes
 def music_duration(time_signature, bars, reciprocal_note_value, tempo):
-  return None
+  SECONDS_PER_MINUTE = 60
+  beats, reciprocal_unity = time_signature.split('/')
+  beats = int(beats)
+  reciprocal_unity = int(reciprocal_unity)
+  unity = 1 / reciprocal_unity
+  note_value = 1 / reciprocal_note_value
+  return bars * beats * note_duration(note_value, unity, tempo) \
+    / SECONDS_PER_MINUTE
 
 assert music_duration("4/4", 10, 4, 60) == 40 / 60, \
   "duration of 40 quarter notes with 60 quarter notes per minute expected, " \
   "found: %s minutes" % music_duration("4/4", 10, 4, 60)
+
+print "OK"
