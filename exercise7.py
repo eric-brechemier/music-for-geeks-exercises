ab2a7f913f68a43ba67da92b655673195293f44e
Eric Bréchemier <eric@bezen.org>
2013-10-07 15:47:09 +0200

    Add answers to exercise 7 and resulting MIDI files
    I named the MIDI files after the sequence of notes generated in each case.
    
    While I initially expected the first file to contain "C D E F G A",
    the two sequences concatenated, and the offset to be inserted between
    each sequence (relative to the end of the last sequence inserted),
    I discovered that the actual file contained only the second sequence.
    
    This helped me to understand that the method seq_notes() inserts all
    sequences from the start of the track (the offset is absolute, not relative)
    and notes from a new sequence may override notes from a previous sequence.
    
    The sequences that I heard in the two other cases confirmed this analysis.

--- exercise7.py
+++ exercise7.py
@@ -16,7 +16,29 @@ from pyknon.music import NoteSeq
 seq1 = NoteSeq("C D E")
 seq2 = NoteSeq("F G A")
 
+# Q. In the following code, what is the order of the notes in the MIDI file?
 midi = Midi()
 midi.seq_notes(seq1)
 midi.seq_notes(seq2)
-midi.write("foo.mid")
+# A. both seq1 and seq2 are written to the same track (1).
+#    The notes of seq2 sequence override notes at the same position in seq1.
+midi.write("exercise7-F-G-A.mid")
+
+# Q.What happens when you change the second-to-last line to
+#   midi.seq_notes(seq2, time=3)
+midi = Midi()
+midi.seq_notes(seq1)
+midi.seq_notes(seq2, time=3)
+# A. Now the sequence seq2 is offset by 3 beats,
+#    the two sequences seq1 and seq2 appear concatenated.
+midi.write("exercise7-C-D-E-F-G-A.mid")
+
+# What happens when you change the second-to-last line to
+#   midi.seq_notes(seq2, time=4)
+midi = Midi()
+midi.seq_notes(seq1)
+midi.seq_notes(seq2, time=4)
+# A. Now the sequence seq2 is offset by 4 beats,
+#    the two sequences seq1 and seq2 appear concatenated,
+#    with a rest of 1 beat in between.
+midi.write("exercise7-C-D-E-R-F-G-A.mid")
