a86a79561b0735f46df26156671b9faba3d46b7e
Eric Bréchemier <eric@bezen.org>
2014-01-17 14:28:20 +0100

    Add parameter to crab_canon for input/output file name
    This will allow to generate multiple crab canons using the same function
    over the course of exercise 11.

--- exercise11.py
+++ exercise11.py
@@ -9,13 +9,18 @@ sys.path.append('./pyknon')
 from pyknon.genmidi import Midi
 from pyknon.music import Note, NoteSeq
 
-def crab_canon():
-    theme2 = NoteSeq("file://exercise11-bach-crab-canon.notes")
+# filename - file name, without extension
+#            The same file name is used as input, with extension .notes,
+#            to read the list of notes for the theme,
+#            and as output, with the extension .mid,
+#            to generate corresponding MIDI file.
+def crab_canon(filename):
+    theme2 = NoteSeq("file://%s.notes" % filename)
     rev_theme = theme2.transposition(-12).retrograde()
 
     midi = Midi(2, tempo=120)
     midi.seq_notes(theme2)
     midi.seq_notes(rev_theme, track=1)
-    midi.write("exercise11-bach-crab-canon.mid")
+    midi.write("%s.mid" % filename)
 
-crab_canon()
+crab_canon("exercise11-bach-crab-canon")
