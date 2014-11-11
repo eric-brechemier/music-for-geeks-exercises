d3491304da1d5408fdf6b9174aba838c9fb3ecf5
Eric Bréchemier <eric@bezen.org>
2014-11-11 16:00:07 +0100

    Complete exercise 13, using lexical order to order and filter sets
    Each chord can be transposed to three different sets (all identical
    in the case of chord (0,4,8)) starting with note 0:
    
    * (0,a,b)
    * (0,b-a,12-a)
    * (0,12-b,12+a-b)
    
    Comparing these three sets using lexical order allows to keep
    only the minimum set, skipping the two variations obtained
    by transposition.
    
    There are 55 sets starting with 0 among the 220 sets of three notes.
    If you set aside (0,4,8) which is identical to itself in each of
    the tree transpositions described above, only one in three sets
    is kept in the remaining 54 sets, which results in 1 + 54/3 = 19 sets.

--- exercise13.py
+++ exercise13.py
@@ -11,26 +11,54 @@ from itertools import combinations
 # using the expression provided in the book
 all_three_note_chords = list(combinations(range(0, 12), 3))
 
+print "%s total distinct sets of three notes:" \
+      % len(all_three_note_chords)
+print all_three_note_chords
+
 assert len(all_three_note_chords) == 220,     "220 three-note chords expected"
 
 # among all distinct sets of 3 notes, sorted in increasing values
 # keep only the minimum sets, which
-# - start with 0
-# - list notes in increasing values of intervals
+# 1) have value 0 for the first note
+# 2) do not match a lower set by shifting the set
+#   to bring the second or the third note down to 0
+#
+# The first condition restricts the selection
+# to sets of the form {0,a,b} with a < b.
+#
+# The second condition requires to compare {0,a,b}
+# with the sets {12-a,a-a,b-a} and {12-b,12+a-b,b-b}.
+# Since 0 < a < b < 12, we have:
+#   * 0 < b-a
+#   * b-a < 12-a
+#   * 12-b < 12-b+a
+#
+# Thus, the two extra sets can be written as:
+# {0,b-a,12-a} and {0,12-b,12+a-b}
+# listing items in increasing order.
+#
+# The set {0,a,b} is lower than both {0,b-a,12-a} and {0,12-b,12+a-b}
+# in lexical order if and only if:
+#   * a < b-a  or ( a == b-a  and b <= 12-a   ),
+# and
+#   * a < 12-b or ( a == 12-b and b <= 12+a-b ).
 def keep_minimum_sets(sets):
-  result=[]
-  for notes in sets:
-    if notes[0] == 0 \
-    and notes[1]-notes[0] <= notes[2]-notes[1] \
-    and notes[2]-notes[1] <= 12-notes[2]:
-      result.append(notes)
-  return result
+    result=[]
+    for notes in sets:
+        if notes[0] != 0:
+            continue
+        a = notes[1]
+        b = notes[2]
+        if  ( a < b-a  or ( a == b-a  and b <= 12-a   ) ) \
+        and ( a < 12-b or ( a == 12-b and b <= 12+a-b ) ):
+            result.append(notes)
+    return result
 
-unrelated_sets_by_transposition_and_inversion = \
+unrelated_sets_by_transposition = \
   keep_minimum_sets(all_three_note_chords)
 
-print "%s sets found unrelated by transposition and inversion" \
-      % len(unrelated_sets_by_transposition_and_inversion)
-print unrelated_sets_by_transposition_and_inversion
-assert len(unrelated_sets_by_transposition_and_inversion) == 12,  \
-                    "12 sets unrelated by transposition and insertion expected"
+print "%s sets found unrelated by transposition:" \
+      % len(unrelated_sets_by_transposition)
+print unrelated_sets_by_transposition
+assert len(unrelated_sets_by_transposition) == 19,  \
+                                 "19 sets unrelated by transposition expected"
