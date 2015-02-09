35c966d05b6e40f9c797dc5fbc96a84098e7d10b
Eric Bréchemier <eric@bezen.org>
2015-02-09 18:33:42 +0100

    Prepare exercise 14: add example code from the book
    The script so far runs the unmodified harmonize() methods
    of Note and NoteSeq and checks that the values correspond
    to those expected (and printed in the book).
    
    In the next step, I will extend the classes Note and NoteSeq,
    adding a new method harmonize_template().
    
    I will then add assertions for the results expected when calling
    harmonize_template() with various templates, including templates
    which correspond to current examples.
    
    Finally, I will add the implementation of harmonize_template()
    and replace the implementation of harmonize() to use
    harmonize_template() instead, and check that all unit tests pass.

---
+++ exercise14.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+# Exercise 14.
+# The method harmonize stacks notes by a fixed interval
+# (by thirds, fourths, etc.).
+# Create a new version that accepts and uses a
+# "chord template" to harmonize a scale.
+
+# Import pyknon from the git submodule in a subdirectory
+import sys
+sys.path.append('./pyknon')
+from pyknon.music import Note, NoteSeq
+
+c = Note("C")
+assert str(c) == '<C>'
+c_major_scale = NoteSeq("C D E F G A B")
+assert str(c_major_scale) == '<Seq: [<C>, <D>, <E>, <F>, <G>, <A>, <B>]>'
+
+assert str(c.harmonize(c_major_scale)) == '[<C>, <E>, <G>]'
+
+assert str(c_major_scale.harmonize()) == '[' \
+        '<Seq: [<C>, <E>, <G>]>, ' \
+        '<Seq: [<D>, <F>, <A>]>, ' \
+        '<Seq: [<E>, <G>, <B>]>, ' \
+        '<Seq: [<F>, <A>, <C>]>, ' \
+        '<Seq: [<G>, <B>, <D>]>, ' \
+        '<Seq: [<A>, <C>, <E>]>, ' \
+        '<Seq: [<B>, <D>, <F>]>' \
+    ']'
+
+assert str(c_major_scale.harmonize(size=4)) == '[' \
+        '<Seq: [<C>, <E>, <G>, <B>]>, ' \
+        '<Seq: [<D>, <F>, <A>, <C>]>, ' \
+        '<Seq: [<E>, <G>, <B>, <D>]>, ' \
+        '<Seq: [<F>, <A>, <C>, <E>]>, ' \
+        '<Seq: [<G>, <B>, <D>, <F>]>, ' \
+        '<Seq: [<A>, <C>, <E>, <G>]>, ' \
+        '<Seq: [<B>, <D>, <F>, <A>]>' \
+    ']'
+
+assert str(c_major_scale.harmonize(interval=4, size=4)) == '[' \
+        '<Seq: [<C>, <F>, <B>, <E>]>, ' \
+        '<Seq: [<D>, <G>, <C>, <F>]>, ' \
+        '<Seq: [<E>, <A>, <D>, <G>]>, ' \
+        '<Seq: [<F>, <B>, <E>, <A>]>, ' \
+        '<Seq: [<G>, <C>, <F>, <B>]>, ' \
+        '<Seq: [<A>, <D>, <G>, <C>]>, ' \
+        '<Seq: [<B>, <E>, <A>, <D>]>' \
+    ']'
+
