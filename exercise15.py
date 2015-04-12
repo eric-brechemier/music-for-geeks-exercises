618f1322fdbb189c11fc2bf2368abdd3cb3cc6ca
Eric Bréchemier <eric@bezen.org>
2015-04-12 15:34:19 +0200

    Add starting point for the exercise 15 in Python
    This is a copy of the utilities defined in this chapter before
    the exercise 15 in the book, with assertions to check that the
    utilities return the values shown in examples.

---
+++ exercise15.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# Exercise 15.
+# Try to construct a scale using only frequencies from the harmonic series.
+# Notice how it sounds different from the equal temperament.
+
+from math import log
+
+SEMITONE = 1.059463
+NOTES = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+
+def freq_to_note(x):
+    interval = int(round(log(x/440.0, SEMITONE))) % 12
+    return NOTES[interval]
+
+def note_to_freq(n):
+    if n in NOTES:
+        return 440 * (SEMITONE ** NOTES.index(n))
+
+assert freq_to_note(440) == 'A'
+assert freq_to_note(442) == 'A'
+assert freq_to_note(449) == 'A'
+assert freq_to_note(455) == 'A#'
+
+assert note_to_freq('A#') == 466.16372
+
+def harmonic_series(n, fundamental):
+    return [fundamental * (x + 1) for x in range(n)]
+
+assert harmonic_series(13,55) == \
+       [55, 110, 165, 220, 275, 330, 385, 440, 495, 550, 605, 660, 715]
